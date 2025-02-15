dc0c09fe67fb6325e46026543d9267fb990f1e2e
Ayushman <ayushman.lakshkar7@gmail.com>
2025-02-15 21:18:15 +0530

    changes to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 import { Code2, Instagram, Phone } from "lucide-react";
 import React from "react";
-
+import logo from '../assets/logo2.png'
 function Footer() {
   return (
     <footer className="py-8 md:py-12 px-4 border-t border-content-subtle/50">
@@ -9,13 +9,12 @@ function Footer() {
         <div className="grid md:grid-cols-3 gap-8 md:gap-12">
           {/* Hidden on Small Screens */}
           <div className="hidden md:block">
-            <div className="flex items-center gap-2 mb-6">
-              <Code2 className="w-8 h-8 text-primary" />
+            <div className="flex items-center mb-6">
+              <img src={logo} className="w-12 h-12" />
               <span className="font-bold text-2xl">CODE COALESCENCE</span>
             </div>
             <p className="text-content-muted text-lg">
-              Where innovation meets reality. Join us for 12 hours of coding,
-              creation, and revolutionary tech.
+            Where innovation meets reality! 🚀 Join us for 12 hours of coding, creation, and groundbreaking tech. 💡🔥
             </p>
           </div>
 
@@ -63,7 +62,7 @@ function Footer() {
                   Shashank Jain: +91 94075 90087
                 </a>
               </li>
-              <li className="flex justify-center md:justify-start items-center gap-2 hover:text-primary transition-colors">
+              {/* <li className="flex justify-center md:justify-start items-center gap-2 hover:text-primary transition-colors">
                 <Phone className="w-5 h-5 text-primary" />
                 <a
                   href="tel:+919130208733"
@@ -71,7 +70,25 @@ function Footer() {
                 >
                   Naman Swarnkar: +91 91302 08733
                 </a>
+              </li> */}
+              <li className="flex justify-center md:justify-start items-center gap-2 hover:text-primary transition-colors">
+                <Phone className="w-5 h-5 text-primary" />
+                <a
+                  href="tel:+916261211162"
+                  className="hover:text-primary transition-colors"
+                >
+                  Karan Mundre: +91 62612 11162
+                </a>
               </li>
+              {/* <li className="flex justify-center md:justify-start items-center gap-2 hover:text-primary transition-colors">
+                <Phone className="w-5 h-5 text-primary" />
+                <a
+                  href="tel:+918871539009"
+                  className="hover:text-primary transition-colors"
+                >
+                  Ayushman Lakshkar: +91 88715 39009
+                </a>
+              </li> */}
               <li className="flex justify-center md:justify-start items-center gap-3 hover:text-primary transition-colors">
                 <Instagram className="w-6 h-6 text-primary" />
                 <a
